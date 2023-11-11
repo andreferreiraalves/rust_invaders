c0a3fdcc4f59af6feb739c23d98395847d17ebfa
André Ferreira <andre.ferreira.alves@gmail.com>
2023-11-11 17:05:18 -0300

    lógina do player e do tiro

---
+++ src/render.rs
@@ -0,0 +1,28 @@
+use crossterm::{
+    cursor::MoveTo,
+    style::{Color, SetBackgroundColor},
+    terminal::{Clear, ClearType},
+    QueueableCommand,
+};
+
+use crate::frame::Frame;
+use std::io::{Stdout, Write};
+
+pub fn render(stdout: &mut Stdout, last_frame: &Frame, curr_frame: &Frame, force: bool) {
+    if force {
+        stdout.queue(SetBackgroundColor(Color::Green)).unwrap();
+        stdout.queue(Clear(ClearType::All)).unwrap();
+        stdout.queue(SetBackgroundColor(Color::Black)).unwrap();
+    }
+
+    for (x, col) in curr_frame.into_iter().enumerate() {
+        for (y, s) in col.into_iter().enumerate() {
+            if *s != last_frame[x][y] || force {
+                stdout.queue(MoveTo(x as u16, y as u16)).unwrap();
+                print!("{}", *s);
+            }
+        }
+    }
+
+    stdout.flush().unwrap();
+}
